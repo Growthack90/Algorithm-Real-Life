e3eaf20d2917ff9300926ce7761c3060b879fa3f
Growthack90 <hackmaker90@outlook.it>
2025-05-17 12:59:15 +0200

    add modif in analisi nutrizionale

--- analisi_nutrizionale/src/main.rs
+++ analisi_nutrizionale/src/main.rs
@@ -13,39 +13,55 @@ fn analyze_nutrition(
     let mut analysis = String::new();
 
     if energy > 250.0 {
-        analysis.push_str("Questo alimento è relativamente alto in calorie. ");
+        analysis.push_str("Questo alimento è relativamente alto in calorie.");
+    } else {
+        analysis.push_str("Questo alimento è relativamente basso in calorie. ");
     }
 
     if fats > 20.0 {
         analysis.push_str("Questo alimento è alto in grassi. Considera di scegliere opzioni con un contenuto di grassi inferiore. ");
     } else if fats < 10.0 {
         analysis.push_str("Questo alimento è basso in grassi. ");
+    } else {
+        analysis.push_str("Questo alimento ha un contenuto di grassi moderato. ");
     }
 
     if saturated_fats > 3.0 {
         analysis.push_str("Questo alimento è alto in grassi saturi. Cerca di limitare i grassi saturi nella tua dieta. ");
+    } else {
+        analysis.push_str("Questo alimento ha un contenuto di grassi saturi moderato. ");
     }
 
     if carbs > 65.0 {
         analysis.push_str("Questo alimento è alto in carboidrati. ");
     } else if carbs < 50.0 {
         analysis.push_str("Questo alimento è basso in carboidrati. ");
+    } else {
+        analysis.push_str("Questo alimento ha un contenuto di carboidrati moderato. ");
     }
 
     if sugars > 25.0 {
         analysis.push_str("Questo alimento è alto in zuccheri. Sii consapevole del tuo consumo di zucchero. ");
+    } else {
+        analysis.push_str("Questo alimento ha un contenuto di zuccheri moderato. ");
     }
 
     if fiber < 6.0 {
         analysis.push_str("Questo alimento è relativamente basso in fibre. Punta a cibi con un contenuto di fibre più elevato. ");
+    } else {
+        analysis.push_str("Questo alimento è una buona fonte di fibre. ");
     }
 
     if protein < 7.0 {
         analysis.push_str("Questo alimento è basso in proteine. ");
+    } else {
+        analysis.push_str("Questo alimento è una buona fonte di proteine. ");
     }
 
     if salt > 0.5 {
         analysis.push_str("Questo alimento è alto in sale. Cerca di limitare l'assunzione di sale. ");
+    } else {
+        analysis.push_str("Questo alimento ha un contenuto di sale moderato. ");
     }
 
     if analysis.is_empty() {
